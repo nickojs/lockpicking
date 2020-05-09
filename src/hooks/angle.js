ed7ab19751aae0f4b67e4d677475f97dcab84e92
nicko <62088609+nickojs@users.noreply.github.com>
2020-05-09 01:41:42 -0300

    Adjustment on the useAngle calculation

--- src/hooks/angle.js
+++ src/hooks/angle.js
@@ -6,21 +6,19 @@ export default (ref) => {
   useEffect(() => {
     // element.offset().left + element.width()/2
     // element.offset().top + element.height()/2
-    const elementWidthHalf = ref.current.offsetWidth / 2;
-    const elementHeightHalf = ref.current.offsetHeight / 2;
+    const elementWidth = ref.current.offsetWidth;
+    const elementHeight = ref.current.offsetHeight;
 
     const elementOffsetLeft = ref.current.getBoundingClientRect().left;
     const elementOffsetTop = ref.current.getBoundingClientRect().top;
 
-    setAngle([elementOffsetLeft + elementWidthHalf, elementOffsetTop + elementHeightHalf]);
+    setAngle([elementOffsetLeft + elementWidth / 2, elementOffsetTop + elementHeight]);
   }, [ref]);
 
   // Math.atan2(e.pageX- angle[0],- (e.pageY- angle[1]) )*(180/Math.PI);
   const returnFinalAngle = (event) => Math.atan2(
-    event.pageX - angle[0],
-    -(event.pageY - angle[1])
-    * (180 / Math.PI)
-  );
+    event.pageX - angle[0], -(event.pageY - angle[1])
+  ) * (180 / Math.PI);
 
   return returnFinalAngle;
 };
