91f6e7331b09ad87762861d98d89a7ebf9c8f89d
nicko <62088609+nickojs@users.noreply.github.com>
2020-05-08 21:59:44 -0300

    Mocking Lockpad styles

--- src/components/Lockpad.js
+++ src/components/Lockpad.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import * as S from './styles';
 
 const lockPad = (props) => (
-  <div>
-    <h1> Hi, i'll be lockpad </h1>
-  </div>
+  <S.Container>
+    <S.LockpadContainer>
+      <h1> Hi, i'll be lockpad </h1>
+    </S.LockpadContainer>
+  </S.Container>
 );
 
 export default lockPad;
---
+++ src/components/styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  margin: 12px auto;
+  padding: 24px;
+`;
+
+export const LockpadContainer = styled.div`
+  width: 400px;
+  height: 400px;
+  background: grey;
+`;
