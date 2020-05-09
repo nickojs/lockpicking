06beb90f7f2948ced9fd0f46fd27042d13450109
nicko <62088609+nickojs@users.noreply.github.com>
2020-05-08 22:44:33 -0300

    Importing images on styles.js to export a 'ready' component

--- src/components/Lockpad.js
+++ src/components/Lockpad.js
@@ -4,7 +4,8 @@ import * as S from './styles';
 const lockPad = (props) => (
   <S.Container>
     <S.LockpadContainer>
-      <h1> Hi, i'll be lockpad </h1>
+      <S.Pick />
+      <S.Lockpad />
     </S.LockpadContainer>
   </S.Container>
 );
--- src/components/styles.js
+++ src/components/styles.js
@@ -1,4 +1,6 @@
 import styled from 'styled-components';
+import fulllock from '../assets/fulllock.png';
+import pick from '../assets/pick.png';
 
 export const Container = styled.div`
   display: flex;
@@ -13,4 +15,24 @@ export const LockpadContainer = styled.div`
   height: 400px;
   padding: 12px;
   background: grey;
+`;
+
+export const Lockpad = styled.img.attrs({
+  src: fulllock,
+  alt: 'an ugly but functional lockpad'
+})`
+  position: absolute;
+  top: 50%; left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 0;
+`;
+
+export const Pick = styled.img.attrs({
+  src: pick,
+  alt: 'a picklock that looks like a twig'
+})`
+  position: absolute;
+  top: -50%; left: 50%;
+  transform: translate(-50%, 0);
+  z-index: 10; 
 `;
