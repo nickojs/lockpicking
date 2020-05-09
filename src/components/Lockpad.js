ac14ba64aded3e7e3da8c7b9bd6c8913a6d7d8bb
nicko <62088609+nickojs@users.noreply.github.com>
2020-05-08 23:06:57 -0300

    Getting the mouse position and saving it to the LockPad state

--- src/components/Lockpad.js
+++ src/components/Lockpad.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import * as S from './styles';
 
-const lockPad = (props) => (
-  <S.Container>
-    <S.LockpadContainer>
-      <S.Pick />
-      <S.Lockpad />
-    </S.LockpadContainer>
-  </S.Container>
-);
+const LockPad = (props) => {
+  const [mousePosition, setMousePosition] = useState({
+    x: null, y: null
+  });
 
-export default lockPad;
+  const mousePositionHandler = (event) => {
+    event.preventDefault();
+    const position = {
+      x: event.nativeEvent.offsetX,
+      y: event.nativeEvent.offsetY
+    };
+    console.log(position);
+    setMousePosition(position);
+  };
+
+  return (
+    <S.Container>
+      <S.LockpadContainer onMouseMove={(event) => mousePositionHandler(event)}>
+        <S.Pick position={mousePosition.x} />
+        <S.Lockpad />
+      </S.LockpadContainer>
+    </S.Container>
+  );
+};
+
+export default LockPad;
