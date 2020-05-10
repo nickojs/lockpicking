9b6a1e4717a9548ffb778962bb55b21b4c9cbfe6
nicko <62088609+nickojs@users.noreply.github.com>
2020-05-09 21:44:24 -0300

    Changing the Lockpad events, due to performance issues

--- src/components/Lockpad.js
+++ src/components/Lockpad.js
@@ -6,17 +6,25 @@ const LockPad = (props) => {
   const pickRef = useRef(null);
   const returnFinalAngle = useAngle(pickRef);
   const [angle, setAngle] = useState(null);
+  const [mouseDown, setMouseDown] = useState(false);
 
-  const mousePositionHandler = (event) => {
-    const currentAngle = returnFinalAngle(event.nativeEvent);
-    if (currentAngle > -115 && currentAngle < 115) {
-      console.log(angle);
-      return setAngle(currentAngle);
+  const setPickPosition = (event) => {
+    if (mouseDown) {
+      const outsideEvent = event.nativeEvent;
+      const currentAngle = returnFinalAngle(outsideEvent);
+      if (currentAngle > -115 && currentAngle < 115) {
+        console.log(angle);
+        return setAngle(currentAngle);
+      }
     }
   };
 
   return (
-    <S.Container onMouseMove={(event) => mousePositionHandler(event)}>
+    <S.Container
+      onMouseDown={() => setMouseDown(true)}
+      onMouseUp={() => setMouseDown(false)}
+      onMouseMove={(event) => setPickPosition(event)}
+    >
       <S.LockpadContainer>
         <S.Pick ref={pickRef} position={angle} />
         <S.Lockpad />
