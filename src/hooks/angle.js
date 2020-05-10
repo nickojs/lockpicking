50c8a026fd3968cf41d9ca2a5a3a8f1f0df032a6
nicko <62088609+nickojs@users.noreply.github.com>
2020-05-09 22:09:38 -0300

    Changed internal logic of useAngle calculation

--- src/hooks/angle.js
+++ src/hooks/angle.js
@@ -1,24 +1,32 @@
 import { useEffect, useState } from 'react';
 
-export default (ref) => {
-  const [angle, setAngle] = useState([]);
+export default (elementRef, event) => {
+  const [internalAngle, setInternalAngle] = useState([]);
 
   useEffect(() => {
     // element.offset().left + element.width()/2
     // element.offset().top + element.height()/2
-    const elementWidth = ref.current.offsetWidth;
-    const elementHeight = ref.current.offsetHeight;
+    const elementWidth = elementRef.current.offsetWidth;
+    const elementHeight = elementRef.current.offsetHeight;
 
-    const elementOffsetLeft = ref.current.getBoundingClientRect().left;
-    const elementOffsetTop = ref.current.getBoundingClientRect().top;
-
-    setAngle([elementOffsetLeft + elementWidth / 2, elementOffsetTop + elementHeight]);
-  }, [ref]);
+    const elementOffsetLeft = elementRef.current.getBoundingClientRect().left;
+    const elementOffsetTop = elementRef.current.getBoundingClientRect().top;
+    console.log('[useAngle]: useEffect fired');
+    setInternalAngle([elementOffsetLeft + elementWidth / 2, elementOffsetTop + elementHeight]);
+  }, [elementRef]);
 
   // Math.atan2(e.pageX- angle[0],- (e.pageY- angle[1]) )*(180/Math.PI);
-  const returnFinalAngle = (event) => Math.atan2(
-    event.pageX - angle[0], -(event.pageY - angle[1])
-  ) * (180 / Math.PI);
+  const returnFinalAngle = () => {
+    if (!event) return;
 
-  return returnFinalAngle;
+    const angle = Math.atan2(
+      event.pageX - internalAngle[0], -(event.pageY - internalAngle[1])
+    ) * (180 / Math.PI);
+
+    if (angle <= -115 || angle >= 115) return null;
+
+    return angle;
+  };
+
+  return returnFinalAngle();
 };
