0abd935db0ec028c493ee63ba6d8a47be2201d00
nicko <62088609+nickojs@users.noreply.github.com>
2020-05-10 01:31:31 -0300

    Adjusted Lockpad to use new Lockpick components, since it was splitted, and also mocking a key press mechanism to turn the lock

--- src/components/Lockpad.js
+++ src/components/Lockpad.js
@@ -6,6 +6,7 @@ import genArray from '../helpers/array-generator';
 const LockPad = () => {
   const [hotzone, setHotzone] = useState([]);
   const [mouseDown, setMouseDown] = useState(false);
+  const [keyDown, setKeyDown] = useState(false);
   const [event, setEvent] = useState(null);
   const pickRef = useRef(null);
   const pickPosition = useAngle(pickRef, event);
@@ -27,14 +28,17 @@ const LockPad = () => {
   return (
     <S.Container
       tabIndex="0"
-      onKeyDown={turnLock}
+      onKeyDown={() => setKeyDown(true)}
+      onKeyUp={() => setKeyDown(false)}
       onMouseDown={() => setMouseDown(true)}
       onMouseUp={() => setMouseDown(false)}
       onMouseMove={setPickPosition}
     >
       <S.LockpadContainer>
         <S.Pick ref={pickRef} position={pickPosition} />
-        <S.Lockpad />
+        <S.LockpadBackground>
+          <S.Lockpad />
+        </S.LockpadBackground>
       </S.LockpadContainer>
     </S.Container>
   );
