ac6fb3a9aec08304eadb3639eabb18df636d9da7
nicko <62088609+nickojs@users.noreply.github.com>
2020-05-11 02:51:13 -0300

    Removed unnecessary case from lockpadReducer

--- src/components/Lockpad/reducers/lockpadReducer.js
+++ src/components/Lockpad/reducers/lockpadReducer.js
@@ -1,19 +1,17 @@
 const actions = {
   SET_HOTZONE: 'SET_HOTZONE',
-  ROTATE_LOCK: 'ROTATE_LOCK',
   UNLOCK: 'UNLOCK'
 };
 
 export const initState = {
   pickOnHotzone: false,
-  rotateLock: false,
+  position: null,
   unlock: false
 };
 
 export const lockpadReducer = (state, action) => {
   switch (action.type) {
-    case actions.SET_HOTZONE: return { ...state, pickOnHotzone: action.status };
-    case actions.ROTATE_LOCK: return { ...state, rotateLock: action.status };
+    case actions.SET_HOTZONE: return { ...state, pickOnHotzone: action.status, position: action.position };
     case actions.UNLOCK: return { ...state, unlock: action.status };
 
     default: throw new Error('[lockpadReducer]: provided action.type is unknown');
