25dab03bba078d7f7111c5ce1c32b2dbcfaa85a4
nicko <62088609+nickojs@users.noreply.github.com>
2020-05-11 03:01:50 -0300

    Added EXIT_HOTZONE action to lockpadReducer

--- src/components/Lockpad/reducers/lockpadReducer.js
+++ src/components/Lockpad/reducers/lockpadReducer.js
@@ -1,17 +1,21 @@
 const actions = {
   SET_HOTZONE: 'SET_HOTZONE',
+  EXIT_HOTZONE: 'EXIT_HOTZONE',
   UNLOCK: 'UNLOCK'
 };
 
 export const initState = {
   pickOnHotzone: false,
-  position: null,
+  distanceFromUnlock: null,
   unlock: false
 };
 
 export const lockpadReducer = (state, action) => {
   switch (action.type) {
-    case actions.SET_HOTZONE: return { ...state, pickOnHotzone: action.status, position: action.position };
+    case actions.SET_HOTZONE: return (
+      { ...state, pickOnHotzone: action.status, distanceFromUnlock: action.distance }
+    );
+    case actions.EXIT_HOTZONE: return { ...state, pickOnHotzone: false };
     case actions.UNLOCK: return { ...state, unlock: action.status };
 
     default: throw new Error('[lockpadReducer]: provided action.type is unknown');
