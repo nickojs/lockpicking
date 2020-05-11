0fffb760ba74ed7460d26c4caf59f305ddb8d941
nicko <62088609+nickojs@users.noreply.github.com>
2020-05-11 06:25:00 -0300

    Added new actions to lockpadReducer

--- src/components/Lockpad/reducers/lockpadReducer.js
+++ src/components/Lockpad/reducers/lockpadReducer.js
@@ -1,25 +1,26 @@
 const actions = {
   SET_HOTZONE: 'SET_HOTZONE',
-  SET_ROTATION: 'SET_ROTATION'
+  CLEAR_HOTZONE: 'CLEAR_HOTZONE',
+  SET_ROTATION: 'SET_ROTATION',
+  CLEAR_ROTATION: 'CLEAR_ROTATION',
+  SET_TURNING: 'SET_TURNING',
+  CLEAR_TURNING: 'CLEAR_TURNING'
 };
 
 export const initState = {
-  pickOnHotzone: false,
+  turning: false,
   distanceFromUnlock: null,
   rotation: 0
 };
 
 const setHotzone = (state, action) => ({
   ...state,
-  pickOnHotzone: action.status,
   distanceFromUnlock: action.distance
 });
 
-const exitHotzone = (state, action) => ({
+const clearHotzone = (state, action) => ({
   ...state,
-  distanceFromUnlock: null,
-  pickOnHotzone: false,
-  rotation: 0
+  ...initState
 });
 
 const setRotation = (state, action) => ({
@@ -27,11 +28,28 @@ const setRotation = (state, action) => ({
   rotation: action.rotation
 });
 
+const clearRotation = (state, action) => ({
+  ...state,
+  rotation: 0
+});
+
+const setTurning = (state, action) => ({
+  ...state,
+  turning: true
+});
+const clearTurning = (state, action) => ({
+  ...state,
+  turning: false
+});
+
 export const lockpadReducer = (state, action) => {
   switch (action.type) {
     case actions.SET_HOTZONE: return setHotzone(state, action);
-    case actions.EXIT_HOTZONE: return exitHotzone(state, action);
+    case actions.CLEAR_HOTZONE: return clearHotzone(state, action);
     case actions.SET_ROTATION: return setRotation(state, action);
+    case actions.CLEAR_ROTATION: return clearRotation(state, action);
+    case actions.SET_TURNING: return setTurning(state, action);
+    case actions.CLEAR_TURNING: return clearTurning(state, action);
 
     default: throw new Error('[lockpadReducer]: provided action.type is unknown');
   }
