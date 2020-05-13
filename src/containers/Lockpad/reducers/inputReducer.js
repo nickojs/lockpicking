045a5d8ef265ca9e138eabd59037e9838490bb5d
nicko <62088609+nickojs@users.noreply.github.com>
2020-05-13 18:26:24 -0300

    Removed clear input action, since it is not possible to do so in a webapp environment

--- src/containers/Lockpad/reducers/inputReducer.js
+++ src/containers/Lockpad/reducers/inputReducer.js
@@ -6,8 +6,7 @@ const actions = {
   INPUT_EVENT: 'INPUT_EVENT',
   KEY_PRESS_START: 'KEY_PRESS_START',
   KEY_PRESS_INC: 'KEY_PRESS_INC',
-  KEY_PRESS_END: 'KEY_PRESS_END',
-  CLEAR_INPUT: 'CLEAR_INPUT'
+  KEY_PRESS_END: 'KEY_PRESS_END'
 };
 
 export const initState = {
@@ -27,7 +26,6 @@ export const inputReducer = (state, action) => {
     case actions.KEY_PRESS_START: return { ...state, keyPressMoment: Date.now() };
     case actions.KEY_PRESS_INC: return { ...state, keyPressMoment: action.inc };
     case actions.KEY_PRESS_END: return { ...state, keyPressMoment: null };
-    case actions.CLEAR_INPUT: return { ...state, ...initState };
     default: throw new Error('[inputReducer]: provided action.type is unknown');
   }
 };
