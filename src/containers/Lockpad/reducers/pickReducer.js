833b3ce24f4024a9433168309c48e9fbe02e4935
nicko <62088609+nickojs@users.noreply.github.com>
2020-05-12 23:18:30 -0300

    Created new actions on Lockpad reducers

--- src/containers/Lockpad/reducers/pickReducer.js
+++ src/containers/Lockpad/reducers/pickReducer.js
@@ -2,13 +2,15 @@ const actions = {
   REMOVE_PICK_LIFE: 'REMOVE_PICK_LIFE',
   SET_UNLOCK: 'SET_UNLOCK',
   SET_GAME_OVER: 'SET_GAME_OVER',
-  CLEAR_GAME: 'CLEAR_GAME'
+  CLEAR_GAME: 'CLEAR_GAME',
+  SET_BROKE_PICK: 'SET_BROKE_PICK'
 };
 
 export const initState = {
   pickLives: 3,
   unlock: false,
-  gameOver: false
+  gameOver: false,
+  pickIsBroken: false
 };
 
 const removePickLife = (state, action) => ({
@@ -31,12 +33,18 @@ const clearPick = (state, action) => ({
   ...initState
 });
 
+const setBrokePick = (state, action) => ({
+  ...state,
+  pickIsBroken: action.status
+});
+
 export const pickReducer = (state, action) => {
   switch (action.type) {
     case actions.REMOVE_PICK_LIFE: return removePickLife(state, action);
     case actions.SET_UNLOCK: return setUnlock(state, action);
     case actions.SET_GAME_OVER: return setGameOver(state, action);
     case actions.CLEAR_GAME: return clearPick(state, action);
+    case actions.SET_BROKE_PICK: return setBrokePick(state, action);
     default: throw new Error('[lockpadReducer]: provided action.type is unknown');
   }
 };
