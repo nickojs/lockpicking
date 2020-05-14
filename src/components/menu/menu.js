c6870826aef554698212d51e446826eef514e00e
nicko <62088609+nickojs@users.noreply.github.com>
2020-05-14 20:06:24 -0300

    Toggling Home menu

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import * as S from './styles';
 
-const menu = (props) => (
-  <nav>
-    <ul>
-      <li>Play</li>
-      <li>Stats</li>
-      <li>Login</li>
-      <li>About</li>
-    </ul>
-  </nav>
-);
+const menu = (props) => {
+  const menu = props.toggle ? (
+    <nav>
+      <ul>
+        <li>Play</li>
+        <li>Stats</li>
+        <li>Login</li>
+        <li>About</li>
+      </ul>
+    </nav>
+  ) : null;
+
+  return menu;
+};
 
 export default menu;
--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -4,10 +4,19 @@ import * as S from './styles';
 
 const Home = (props) => {
   const [keyIdentifier, setKeyIdentifier] = useState(null);
+  const [menuToggle, setMenuToggle] = useState(false);
 
   const keyDownHandler = ({ nativeEvent }) => {
     const { keyCode } = nativeEvent;
     setKeyIdentifier(keyCode);
+    if (keyCode === 32 && keyIdentifier === 32) {
+      setKeyIdentifier(null);
+      setMenuToggle(false);
+      return;
+    }
+    if (keyCode === 32) {
+      return setMenuToggle(true);
+    }
   };
 
   return (
@@ -17,7 +26,7 @@ const Home = (props) => {
         <S.Text>lockpick simulator</S.Text>
         <hr />
         <S.TextSmall>Press any key to start</S.TextSmall>
-        <Menu keyPressed={keyIdentifier} />
+        <Menu keyPressed={keyIdentifier} toggle={menuToggle} />
       </S.InnerContainer>
     </S.Container>
   );
