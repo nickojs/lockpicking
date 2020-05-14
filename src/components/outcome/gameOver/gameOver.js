d787ad22b38b12dd18325c40d5daf7a955e34a9d
nicko <62088609+nickojs@users.noreply.github.com>
2020-05-14 06:04:13 -0300

    Created mechanism to load random termination phrases on the gameOver screen. Similar to those hypocritical, self-righteous HR emails

--- src/components/outcome/gameOver/gameOver.js
+++ src/components/outcome/gameOver/gameOver.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import answers from '../text.json';
 
-const gameOver = (props) => (
-  <>
-    <h1>
-      We appreciate that you took the time to unlock this chest,
-      but unfortunately we&apos;ll move on with another applicant.
-    </h1>
-  </>
-);
+const gameOver = (props) => {
+  // this will be replaced when backend is implemented
+  const generateRandomNumber = (min, max) => Math.ceil(Math.random() * (max - min) + min);
+
+  const generatePhrase = () => {
+    const randomNumber = generateRandomNumber(0, 6);
+    return answers.gameOver[randomNumber];
+  };
+
+  const randomPhrase = generatePhrase();
+
+  return (
+    <>
+      <h1>
+        {randomPhrase}
+      </h1>
+    </>
+  );
+};
+
 
 export default gameOver;
