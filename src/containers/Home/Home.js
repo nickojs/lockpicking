0a418c17636288974138dd70e018022691186b31
nicko <62088609+nickojs@users.noreply.github.com>
2020-05-14 19:30:55 -0300

    Styled Home container

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import Menu from '../../components/menu/menu';
+import * as S from './styles';
 
 const Home = (props) => (
-  <div>
-    <h1>Skyrim</h1>
-    <p>lockpick simulator</p>
-    <p>Press any key to start</p>
-    <Menu />
-  </div>
+  <S.Container>
+    <S.InnerContainer>
+      <S.Title>Skyrim</S.Title>
+      <S.Text>lockpick simulator</S.Text>
+      <hr />
+      <S.TextSmall>Press any key to start</S.TextSmall>
+      <Menu />
+    </S.InnerContainer>
+  </S.Container>
 );
 
 export default Home;
---
+++ src/containers/Home/styles.js
@@ -0,0 +1,39 @@
+import styled from 'styled-components';
+import homeBackground from '../../assets/background.jpg';
+
+export const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  background: url(${homeBackground});
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  text-align: center;
+`;
+
+export const InnerContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding-top: 5em;
+  color: white;
+`;
+
+export const Title = styled.h1`
+  font-size: 4em;
+  font-weight: 700;
+  text-shadow: 1px 1px black;
+`;
+
+export const Text = styled.p`
+  font-size: 2em;
+  font-weight: 700;
+  text-shadow: 1px 1px black;
+`;
+
+export const TextSmall = styled.p`
+  font-size: 1em;
+  font-weight: 400;
+  text-shadow: 1px 1px black;
+`;
