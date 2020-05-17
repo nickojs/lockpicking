2492608f9a8ea00651ae7a0edcaa0fd4b83643fb
nicko <62088609+nickojs@users.noreply.github.com>
2020-05-17 03:57:15 -0300

    LockpadMenu container now deals with logic, progressBar just renders

--- src/containers/LockpadMenu/LockpadMenu.js
+++ src/containers/LockpadMenu/LockpadMenu.js
@@ -1,25 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as S from './styles';
 import ProgressBar from './progressBar/progressBar';
 
-const LockpadMenu = () => (
-  <S.OptionsContainer>
-    <S.OptionsMenu>
-      <S.SecondaryTitle>How to play</S.SecondaryTitle>
-      <S.HowToList>
-        <li>Click and hold the pick to move it</li>
-        <li>Press and hold any key unlock</li>
-        <li>Don&apos;t hush it! Otherwise you&apos;ll jam the lock.</li>
-      </S.HowToList>
+const LockpadMenu = () => {
+  const [difficulty, setDifficulty] = useState(0);
 
-      <S.SecondaryTitle>Set difficulty</S.SecondaryTitle>
-      <S.DifficultyContainer>
-        <ProgressBar />
-        <S.Button to="/game">Play</S.Button>
-      </S.DifficultyContainer>
+  const changeDifficultyHandler = ({ nativeEvent }) => {
+    setDifficulty(nativeEvent.target.value);
+  };
 
-    </S.OptionsMenu>
-  </S.OptionsContainer>
-);
+  const roundedDifficulty = Math.floor(difficulty);
+
+  return (
+    <S.OptionsContainer>
+      <S.OptionsMenu>
+        <S.SecondaryTitle>How to play</S.SecondaryTitle>
+        <S.HowToList>
+          <li>Click and hold the pick to move it</li>
+          <li>Press and hold any key unlock</li>
+          <li>Don&apos;t hush it! Otherwise you&apos;ll jam the lock.</li>
+        </S.HowToList>
+
+        <S.SecondaryTitle>Set difficulty</S.SecondaryTitle>
+        <S.DifficultyContainer>
+          <ProgressBar
+            difficulty={roundedDifficulty}
+            difficultyHandler={changeDifficultyHandler}
+          />
+          <S.Button to={{
+            pathname: '/game',
+            state: {
+              difficulty,
+              startingTime: Date.now()
+            }
+          }}
+          >Play
+          </S.Button>
+        </S.DifficultyContainer>
+
+      </S.OptionsMenu>
+    </S.OptionsContainer>
+  );
+};
 
 export default LockpadMenu;
--- src/containers/LockpadMenu/progressBar/progressBar.js
+++ src/containers/LockpadMenu/progressBar/progressBar.js
@@ -1,28 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import * as S from './styles';
 
-const ProgressBar = (props) => {
-  const [difficulty, setDifficulty] = useState(0);
-
-  const changeDifficultyHandler = ({ nativeEvent }) => {
-    setDifficulty(nativeEvent.target.value);
-  };
-
-  const roundedDifficulty = Math.floor(difficulty);
-
-  return (
-    <S.ProgressContainer>
-      <S.DifficultyMeter>{roundedDifficulty}</S.DifficultyMeter>
-      <S.ProgressBar
-        type="range"
-        min="0"
-        max="10"
-        step="0.5"
-        onChange={changeDifficultyHandler}
-        value={difficulty}
-      />
-    </S.ProgressContainer>
-  );
-};
+const ProgressBar = ({ difficulty, difficultyHandler }) => (
+  <S.ProgressContainer>
+    <S.DifficultyMeter>{difficulty}</S.DifficultyMeter>
+    <S.ProgressBar
+      type="range"
+      min="0"
+      max="10"
+      step="0.5"
+      onChange={difficultyHandler}
+      value={difficulty}
+    />
+  </S.ProgressContainer>
+);
 
 export default ProgressBar;
