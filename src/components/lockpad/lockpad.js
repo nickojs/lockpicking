23c4f2f9262d7429983bba629a593154d8604b21
nicko <62088609+nickojs@users.noreply.github.com>
2020-05-19 14:43:56 -0300

    Added background to the lockpad component, adjust elements positioning

--- src/components/lockpad/lockpad.js
+++ src/components/lockpad/lockpad.js
@@ -4,13 +4,15 @@ import * as S from './styles';
 const lockpad = ({
   rotation, turning, pickRef, pickPosition
 }) => (
-  <S.LockpadContainer position={rotation} isTurning={turning}>
-    <S.Pick ref={pickRef} position={pickPosition} />
+  <S.LockBackground>
+    <S.LockpadContainer position={rotation} isTurning={turning}>
+      <S.Pick ref={pickRef} position={pickPosition} />
 
-    <S.LockpadBackground>
-      <S.Lockpad />
-    </S.LockpadBackground>
-  </S.LockpadContainer>
+      <S.LockpadBackground>
+        <S.Lockpad />
+      </S.LockpadBackground>
+    </S.LockpadContainer>
+  </S.LockBackground>
 );
 
 export default lockpad;
--- src/components/lockpad/styles.js
+++ src/components/lockpad/styles.js
@@ -1,16 +1,32 @@
 import styled from 'styled-components';
-import lockhole from '../../assets/lockpad/lockhole.png';
 import lockpadBackground from '../../assets/lockpad/lockpad_background.png';
+import lockBackground from '../../assets/lockpad/lockbackground.png';
+
+import lockhole from '../../assets/lockpad/lockhole.png';
 import pick from '../../assets/lockpad/pick_with_space.png';
+
+export const LockBackground = styled.div`
+  position: relative;
+  height: 474px;
+  width: 569px;
+
+  background: url(${lockBackground});
+  background-size: 100%;
+  background-position: center;
+  background-repeat: no-repeat;
+`;
 
 export const LockpadContainer = styled.div.attrs((props) => (props.isTurning ? ({
   style: {
     transform: `rotate(${props.position}deg)`
   }
 }) : null))`
-  position: relative;
-  width: 400px;
-  height: 400px;
+  position: absolute;
+  top: 20%;
+  left: 35%;
+  transform: rotate(0deg);
+  width: 300px;
+  height: 300px;
   transition: .5s;
 `;
 
@@ -21,19 +37,16 @@ export const LockpadBackground = styled.div`
 
   width: 100%;
   height: 100%;  
-
   background: url(${lockpadBackground});
   background-position: center;
-  background-size: 80%;
+  background-size: 100%;
   background-repeat: no-repeat;
 `;
 
 export const Lockpad = styled.img.attrs({
   src: lockhole,
   alt: 'an ugly but functional lockpad'
-})`
-  width: 90px;
-`;
+})` width: 90px; `;
 
 export const Pick = styled.img.attrs((props) => ({
   src: pick,
@@ -45,7 +58,7 @@ export const Pick = styled.img.attrs((props) => ({
   cursor: none;
   pointer-events: none;
   position: absolute;
-  top: -73%; left: 50%;
+  top: calc(36% - 448px); left: 50%;
   transform-origin: 0 100%;
   transform: rotate(0deg) translate(-50%, 0%);
   z-index: 10; 
