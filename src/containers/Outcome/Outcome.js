3255dc79bbe44f76059a482ac48f26686e99d848
nicko <62088609+nickojs@users.noreply.github.com>
2020-05-20 05:52:42 -0300

    Removed helper comment, comment Share button for now

--- src/containers/Outcome/Outcome.js
+++ src/containers/Outcome/Outcome.js
@@ -6,14 +6,6 @@ import GameOver from './gameOver/gameOver';
 import Dialog from '../../components/dialog/dialog';
 
 const Outcome = ({ location }) => {
-  /*
-    will receive following props:
-      - how many picks was used
-      - how much time spend on the lockpad
-      - 'unlocked' or 'gameOver'
-    if 'unlocked', will render Unlocked component
-    if 'gameOver', will render GameOver component
-  */
   const { gameOver, unlock, stats } = location.state;
   const gameover = gameOver ? <GameOver /> : null;
   const unlocked = unlock ? <Unlocked stats={stats} /> : null;
@@ -25,7 +17,7 @@ const Outcome = ({ location }) => {
         {gameover}
         <S.Navigation>
           <S.Button to="/">Return</S.Button>
-          <p>Share</p>
+          {/* <p>Share</p> */}
         </S.Navigation>
       </Dialog>
     </S.Container>
