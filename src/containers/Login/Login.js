484107664759348421d2fb70853983bd0de42f4c
nicko <62088609+nickojs@users.noreply.github.com>
2020-05-24 16:18:37 -0300

    Added react-hook-form to the project, start mocking the login form

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -1,8 +1,28 @@
 import React from 'react';
-import * as S from './styles';
+import { useForm } from 'react-hook-form';
 
-const Login = () => (
-  <S.Container />
-);
+import * as S from './styles';
+import Dialog from '../../components/dialog/dialog';
+
+const Login = () => {
+  const { register, handleSubmit, errors } = useForm();
+  const onSubmit = (data) => console.log(data);
+  console.log(errors);
+
+  return (
+    <S.Container>
+      <S.Title>Wait.. I know you</S.Title>
+      <Dialog>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <S.Input type="text" placeholder="username" name="username" ref={register({ required: true, minLength: 4, maxLength: 18 })} />
+          <S.Input type="text" placeholder="password" name="password" ref={register({ required: true, minLength: 8, maxLength: 20 })} />
+          <S.Input type="email" placeholder="email" name="email" ref={register({ required: true })} />
+
+          <input type="submit" />
+        </form>
+      </Dialog>
+    </S.Container>
+  );
+};
 
 export default Login;
--- src/containers/Login/styles.js
+++ src/containers/Login/styles.js
@@ -4,13 +4,30 @@ import bg from '../../assets/image/login.jpg';
 export const Container = styled.div`
   display: flex;
   justify-content: center;
-  align-items: center;
+  align-items: flex-start;
   height: 100vh;
   width: 100vw;
   user-select: none;
+  color: white;
 
   background: url(${bg});
   background-position: center;
   background-repeat: no-repeat;
   background-size: cover;
+`;
+
+export const Title = styled.h1`
+  font-size: 4em;
+  font-weight: 700;
+  text-shadow: 1px 1px black;
+`;
+
+export const Input = styled.input`
+  padding: 12px;
+
+  border: none;
+  border-bottom: 1px solid white;
+
+  color: white;
+  background: transparent;
 `;
