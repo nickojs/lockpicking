6bd4643472d6663182abc73a16b51b66a177a89d
nicko <62088609+nickojs@users.noreply.github.com>
2020-05-24 20:49:23 -0300

    Mocking sideMenu with multiple forms, still needs adjusments

--- src/components/sideMenu/sideMenu.js
+++ src/components/sideMenu/sideMenu.js
@@ -8,7 +8,6 @@ const SideMenu = ({ toggle, index, clicked }) => {
         <S.SideItems>
           <S.Item active={index === 0 ? 1 : 0}>Create Account</S.Item>
           <S.Item active={index === 1 ? 1 : 0}>Login</S.Item>
-          <S.Item active={index === 2 ? 1 : 0}>Return</S.Item>
         </S.SideItems>
       </S.SideMenu>
     </S.Container>
--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useForm } from 'react-hook-form';
 
 import * as S from './styles';
@@ -9,11 +9,14 @@ import Dialog from '../../components/dialog/dialog';
 
 const Login = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
+  const [triggerForm, setTriggerForm] = useState(false);
   const [index, setIndex] = useState(0);
+  const [form, setForm] = useState(null);
   const container = useRef(null);
-  // const { register, handleSubmit, errors } = useForm();
-  // const onSubmit = (data) => console.log(data);
-  // console.log(errors);
+
+  const { register, handleSubmit, errors } = useForm();
+  const onSubmit = (data) => console.log(data);
+  console.log(errors);
 
 
   const keyDownHandler = ({ nativeEvent }) => {
@@ -31,6 +34,11 @@ const Login = () => {
         case 40:
           if (index < 2) setIndex((prevIndex) => prevIndex + 1);
           break;
+        case 68:
+        case 39:
+          setTriggerForm(true);
+          setToggleMenu(false);
+          break;
         default:
           break;
       }
@@ -42,6 +50,49 @@ const Login = () => {
     container.current.focus();
   };
 
+  useEffect(() => {
+    console.log('form useEffect');
+
+    switch (index) {
+      case null: return;
+      case 0:
+        setForm((
+          <Dialog>
+            <S.SmallTitle>You&apos;re finally awake</S.SmallTitle>
+            <S.Form onSubmit={handleSubmit(onSubmit)}>
+              <S.FormInputs>
+                <S.Input type="text" placeholder="username" name="username" ref={register({ required: true, minLength: 4, maxLength: 18 })} />
+                <S.Input type="email" placeholder="email" name="email" ref={register({ required: true })} />
+                <S.Input type="text" placeholder="password" name="password" ref={register({ required: true, minLength: 8, maxLength: 20 })} />
+              </S.FormInputs>
+              <S.FormSubmit>
+                <S.ConfirmButton type="submit" value="Create Account" />
+              </S.FormSubmit>
+            </S.Form>
+          </Dialog>
+        ));
+        break;
+      case 1:
+        setForm((
+          <Dialog>
+            <S.SmallTitle>Wait... I know you</S.SmallTitle>
+            <S.Form onSubmit={handleSubmit(onSubmit)}>
+              <S.FormInputs>
+                <S.Input type="text" placeholder="username" name="username" ref={register({ required: true, minLength: 4, maxLength: 18 })} />
+                <S.Input type="text" placeholder="password" name="password" ref={register({ required: true, minLength: 8, maxLength: 20 })} />
+              </S.FormInputs>
+              <S.FormSubmit>
+                <S.ConfirmButton type="submit" value="Login" />
+              </S.FormSubmit>
+            </S.Form>
+          </Dialog>
+        ));
+        break;
+      default:
+        break;
+    }
+  }, [handleSubmit, index, register]);
+
   return (
     <>
       <S.Container
@@ -52,19 +103,7 @@ const Login = () => {
       >
         <Title>Login</Title>
         <TextSmall>Press space to open menu</TextSmall>
-        {/* <Dialog>
-        <S.SmallTitle>Wait... I know you</S.SmallTitle>
-        <S.Form onSubmit={handleSubmit(onSubmit)}>
-          <S.FormInputs>
-            <S.Input type="text" placeholder="username" name="username" ref={register({ required: true, minLength: 4, maxLength: 18 })} />
-            <S.Input type="email" placeholder="email" name="email" ref={register({ required: true })} />
-            <S.Input type="text" placeholder="password" name="password" ref={register({ required: true, minLength: 8, maxLength: 20 })} />
-          </S.FormInputs>
-          <S.FormSubmit>
-            <S.ConfirmButton type="submit" value="Login" />
-          </S.FormSubmit>
-        </S.Form>
-      </Dialog> */}
+        {triggerForm && form}
       </S.Container>
       <SideMenu
         toggle={toggleMenu}
