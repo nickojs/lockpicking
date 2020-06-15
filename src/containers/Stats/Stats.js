9e82be4e3567a00a40bb09c1932983bfc2f6137f
nicko <62088609+nickojs@users.noreply.github.com>
2020-06-15 13:14:03 -0300

    Mocking Stats page

---
+++ src/containers/Stats/Stats.js
@@ -0,0 +1,16 @@
+import React, { useEffect } from 'react';
+import * as S from './styles';
+
+const Stats = () => {
+  useEffect(() => {
+    //
+  }, []);
+
+  return (
+    <S.Container>
+      <h1>Hi</h1>
+    </S.Container>
+  );
+};
+
+export default Stats;
---
+++ src/containers/Stats/styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+import background from '../../assets/image/home.jpg';
+import { Container as C } from '../../generalStyles';
+
+export const Container = styled(C)`
+  background: url(${background});
+`;
