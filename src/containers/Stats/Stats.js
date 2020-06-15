28f84ba5f86161bd5a8acfa87d43fc89b70f65bd
nicko <62088609+nickojs@users.noreply.github.com>
2020-06-15 15:24:51 -0300

    Finish developing stats container

--- src/containers/Stats/Stats.js
+++ src/containers/Stats/Stats.js
@@ -1,14 +1,62 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import moment from 'moment';
+
 import * as S from './styles';
+import { Title, InnerContainer } from '../../generalStyles';
+import useRequest from '../../hooks/request';
 
 const Stats = () => {
+  const [options, setOptions] = useState({});
+  const [requestData] = useRequest(options);
+  const { data, loading, error } = requestData;
+
   useEffect(() => {
-    //
+    setOptions({
+      method: 'GET',
+      url: `https://${process.env.REACT_APP_BACKEND}/stats`
+    });
   }, []);
+
+  let stats;
+
+  if (data) {
+    stats = data.records.map((s) => (
+      <tr key={s.id}>
+        <td>{s.username}</td>
+        <td>{Number(s.time).toFixed(2)}</td>
+        <td>{s.picks}</td>
+        <td>{moment(s.createdAt).format('MMMM Do [at] h:mm:ss a')}</td>
+      </tr>
+    ));
+  }
 
   return (
     <S.Container>
-      <h1>Hi</h1>
+      <InnerContainer>
+        <Title>Stats</Title>
+      </InnerContainer>
+      <InnerContainer>
+        <S.MsgContainer>
+          {error && <S.ErrorMsg>{error}</S.ErrorMsg> }
+          {loading && <p>Loading...</p>}
+        </S.MsgContainer>
+        {data
+          && (
+          <S.Table>
+            <thead>
+              <tr>
+                <th>username</th>
+                <th>time</th>
+                <th>picks</th>
+                <th>date</th>
+              </tr>
+            </thead>
+            <tbody>
+              {stats}
+            </tbody>
+          </S.Table>
+          )}
+      </InnerContainer>
     </S.Container>
   );
 };
--- src/containers/Stats/styles.js
+++ src/containers/Stats/styles.js
@@ -3,5 +3,48 @@ import background from '../../assets/image/home.jpg';
 import { Container as C } from '../../generalStyles';
 
 export const Container = styled(C)`
+  justify-content: flex-start;
+  flex-direction: column;
+  text-align: center;
+  
+  color: white;
   background: url(${background});
+`;
+
+export const MsgContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  p{ 
+    margin: 24px auto;
+  }
+`;
+
+export const ErrorMsg = styled.p`
+  margin: 6px auto;
+
+  font-family: 'Arial', sans-serif;
+  font-size: .8em;
+
+  text-shadow: 0px 0px 8px lightcoral;
+  text-align: center;
+  
+  color: red;
+`;
+
+export const Table = styled.table`
+  
+  border: 5px double #EEE;
+  background: rgba(0, 0, 0, 0.9);
+
+  td, th{ 
+    padding: 8px; 
+    border: 1px solid #EEE;
+  }
+
+  tbody tr:hover{ 
+    color: black;
+    background: #CCC; 
+  }
 `;
