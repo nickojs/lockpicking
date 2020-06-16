264c65cd418dbb693ed91de137c9547fe4169160
nicko <62088609+nickojs@users.noreply.github.com>
2020-06-16 17:44:23 -0300

    adjusting hint styles

--- src/components/hint/styles.js
+++ src/components/hint/styles.js
@@ -2,11 +2,14 @@ import styled from 'styled-components';
 
 export const HintContainer = styled.div`
   position: absolute;
-  top: 1%; left: 50%;
+  top: 5%; left: 50%;
   transform: translate(-50%);
   width: 100%;
+  z-index: 90;
 `;
 
 export const HintText = styled.p`
   margin: 12px;
+  color: white;
+  text-align: center;
 `;
