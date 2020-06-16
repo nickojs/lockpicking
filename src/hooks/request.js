4dba29aeba1e48ebdc36b63e96e7a739fe580e9f
nicko <62088609+nickojs@users.noreply.github.com>
2020-06-16 18:33:51 -0300

    returning full error object

--- src/hooks/request.js
+++ src/hooks/request.js
@@ -52,7 +52,7 @@ export default (options) => {
       dispatch({ type: 'DATA', data: request.data });
     } catch (err) {
       if (err.response) {
-        dispatch({ type: 'ERROR', error: err.response.data.error });
+        dispatch({ type: 'ERROR', error: err.response.data });
       } else if (err.request) {
         dispatch({ type: 'ERROR', error: 'Couldn\'t reach server.' });
       } else {
