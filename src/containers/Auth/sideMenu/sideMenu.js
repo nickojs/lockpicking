8a020a34d13e1b387d918c87638f087a20434543
nicko <62088609+nickojs@users.noreply.github.com>
2020-06-16 17:44:31 -0300

    imported hint on sideMenu

--- src/containers/Auth/sideMenu/sideMenu.js
+++ src/containers/Auth/sideMenu/sideMenu.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import * as S from './styles';
+import Hint from '../../../components/hint/hint';
 
 const SideMenu = ({ toggle, index, clicked }) => {
   const menu = toggle && (
-    <S.Container onClick={clicked}>
-      <S.SideMenu>
-        <S.SideItems>
-          <S.Item active={index === 0 ? 1 : 0}>Create Account</S.Item>
-          <S.Item active={index === 1 ? 1 : 0}>Login</S.Item>
-          <S.Item active={index === 2 ? 1 : 0}>Request Token</S.Item>
-          <S.Item active={index === 3 ? 1 : 0}>Update Account</S.Item>
-        </S.SideItems>
-      </S.SideMenu>
-    </S.Container>
+  <S.Container onClick={clicked}>
+    <Hint>
+      Use the WASD keys (or arrows) to control the menu <br />
+      Press D (or left arrow) to open the selected menu
+    </Hint>
+    <S.SideMenu>
+      <S.SideItems>
+        <S.Item active={index === 0 ? 1 : 0}>Create Account</S.Item>
+        <S.Item active={index === 1 ? 1 : 0}>Login</S.Item>
+        <S.Item active={index === 2 ? 1 : 0}>Request Token</S.Item>
+        <S.Item active={index === 3 ? 1 : 0}>Update Account</S.Item>
+      </S.SideItems>
+    </S.SideMenu>
+  </S.Container>
   );
 
   return menu;
